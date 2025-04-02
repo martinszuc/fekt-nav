a1adab9a61c032b91305cce75b24fd449bc7ee6a
Student <Student@PC-SE5113-01.utko.feec.vutbr.cz>
2025-04-02 13:40:37 +0200

    cv7: okna finished

--- Uloha07/okna_kostra.asm
+++ Uloha07/okna_kostra.asm
@@ -1,17 +1,13 @@
-; Autor: Miroslav Balik
+; Autor: Martin Szuc (ID: 231284)
 ; Source code: EXE(OBJ) 32bit Win32 API
 ; Directs for assembling and linking:
 ; nasm okna.asm -fobj
 ; alink okna.obj -oPE -subsys gui
 
-; sablonu je potreba prejmenovat na okna.asm
+%include 'win32n.inc'	; definicie struktur a roznych datovych typov
 
 
-
-%include 'win32n.inc'	;definice struktur a ruznych datovych typu
-
-
-; importing extern functions
+; importovanie externych funkcii
 extern ExitProcess
 import ExitProcess kernel32.dll
 extern GetModuleHandle
@@ -45,134 +41,186 @@ import MessageBox user32.dll MessageBoxA
 
 
 
-;Data segment
-;**************************************************************************
+; Datovy segment
+; **************************************************************************
 [section .data class=DATA use32 align=16]
 
-; text strings
+; textove retazce
 szWndClassName: DB "WIN CLASS32",0
 ClassNameStatButton: DB "BUTTON",0
 szCButn: DB "Dialogove okno...",0
 cap_dialog: DB "Nadpis dialogove okno.",0
 mes_dialog: DB "Text dialogoveho okna.",0
-szWndCaption: DB "okno",0
+szWndCaption: DB "Okno - CV7 Martin Szuc",0
 
-;handle:
-hInstance		dd		0		; instanc handle
-hWnd			dd		0 		; window handle
-hButton			dd		0 		; window handle
+; handly
+hInstance       dd      0       ; instanc handle
+hWnd            dd      0       ; window handle
+hButton         dd      0       ; button handle
 
-;global variables
-Message: 		resb 	MSG_size	;alokace mista v pameti pro zpravu
+; globalne premenne
+Message:        resb    MSG_size    ; alokacia miesta v pamati pre spravu
 
-;Definiton WndClass structure
+; Definicia struktury WndClass
 WndClass:
-	istruc WNDCLASSEX
-		at WNDCLASSEX.cbSize, dd WNDCLASSEX_size
-		at WNDCLASSEX.style, dd CS_VREDRAW+ CS_HREDRAW + CS_GLOBALCLASS
-		at WNDCLASSEX.lpfnWndProc, dd WndProc
-		at WNDCLASSEX.cbClsExtra, dd 0
-		at WNDCLASSEX.cbWndExtra, dd 0
-		at WNDCLASSEX.hInstance, dd NULL
-		at WNDCLASSEX.hIcon, dd NULL
-		at WNDCLASSEX.hCursor, dd NULL
-		at WNDCLASSEX.hbrBackground, dd COLOR_BACKGROUND
-		at WNDCLASSEX.lpszMenuName, dd NULL
-		at WNDCLASSEX.lpszClassName, dd szWndClassName
-		at WNDCLASSEX.hIconSm, dd NULL
-	iend
+    istruc WNDCLASSEX
+        at WNDCLASSEX.cbSize, dd WNDCLASSEX_size
+        at WNDCLASSEX.style, dd CS_VREDRAW+ CS_HREDRAW + CS_GLOBALCLASS
+        at WNDCLASSEX.lpfnWndProc, dd WndProc
+        at WNDCLASSEX.cbClsExtra, dd 0
+        at WNDCLASSEX.cbWndExtra, dd 0
+        at WNDCLASSEX.hInstance, dd NULL
+        at WNDCLASSEX.hIcon, dd NULL
+        at WNDCLASSEX.hCursor, dd NULL
+        at WNDCLASSEX.hbrBackground, dd COLOR_BACKGROUND
+        at WNDCLASSEX.lpszMenuName, dd NULL
+        at WNDCLASSEX.lpszClassName, dd szWndClassName
+        at WNDCLASSEX.hIconSm, dd NULL
+    iend
 
-;Code segment
-;***************************************************************************
+; Kodovy segment
+; ***************************************************************************
 [section .code use32 class=CODE]
 
 ..start:
+    ; Ziskanie handle instancie
+    push dword NULL
+    call [GetModuleHandle]
+    mov [hInstance], eax
+    
+    ; Nastavenie ikon a kurzorov
+    push dword IDI_APPLICATION
+    push dword NULL
+    call [LoadIcon]
+    mov [WndClass + WNDCLASSEX.hIcon], eax
+    mov [WndClass + WNDCLASSEX.hIconSm], eax
+    
+    push dword IDC_ARROW
+    push dword NULL
+    call [LoadCursor]
+    mov [WndClass + WNDCLASSEX.hCursor], eax
+    
+    ; Nastavenie handlu instancie
+    mov eax, [hInstance]
+    mov [WndClass + WNDCLASSEX.hInstance], eax
+    
+    ; Registracia triedy okna
+    push dword WndClass
+    call [RegisterClassEx]
+    test eax, eax
+    jz near .Finish
+    
+    ; Vytvorenie hlavneho okna
+    push dword NULL
+    push dword [hInstance]
+    push dword NULL
+    push dword NULL
+    push dword 275
+    push dword 300
+    push dword 100
+    push dword 100
+    push dword WS_OVERLAPPED | WS_CAPTION | WS_SYSMENU | WS_MINIMIZEBOX
+    push dword szWndCaption
+    push dword szWndClassName
+    push dword 0
+    call [CreateWindowEx]
+    test eax, eax
+    jz near .Finish
+    mov [hWnd], eax
+    
+    ; Zobrazenie okna
+    push dword SW_SHOWNORMAL
+    push dword [hWnd]
+    call [ShowWindow]
 
 
-
-
-;	push dword NULL
-;	push dword [hInstance]
-;	push dword NULL
-;	push dword NULL
-;	push dword 275
-;	push dword 300
-;	push dword 100
-;	push dword 100
-;	push dword WS_OVERLAPPED | WS_CAPTION | WS_SYSMENU | WS_MINIMIZEBOX
-;	push dword szWndCaption
-;	push dword szWndClassName
-;	push dword 0
-;	call [CreateWindowEx]
-
-
-
-
-; ------------------------- MESSAGE LOOP ---------------------------------
+; ------------------------- SLUCKA SPRAV ---------------------------------
 .MessageLoop:
-	push dword 0
-	push dword 0
- 	push dword NULL
- 	push dword Message
- 	call [GetMessage]
-	test eax,eax  
-	jz near .Finish
-	cmp eax,-1 
-	jz near .Finish
+    push dword 0
+    push dword 0
+    push dword NULL
+    push dword Message
+    call [GetMessage]
+    test eax,eax  
+    jz near .Finish
+    cmp eax,-1 
+    jz near .Finish
 
-	push dword Message
- 	call [TranslateMessage] 
- 	push dword Message
- 	call [DispatchMessage]  
-	jmp .MessageLoop
-	
+    push dword Message
+    call [TranslateMessage] 
+    push dword Message
+    call [DispatchMessage]  
+    jmp .MessageLoop
+    
 
 .Finish:
-	push dword NULL
-	call [ExitProcess]
-	
-; ---------------------- WINDOW PROCEDURE ---------------------------------
-	WndProc:
-		WndProc_PARAMBYTES EQU 16
-		enter 0,0
-		
-		mov eax,dword [ebp+12]
-		cmp eax,WM_CREATE
-			je near .Create
-		cmp eax,WM_DESTROY
-			je near .Destroy
-		cmp eax,WM_CLOSE
-			je near .Destroy
-		cmp eax,WM_COMMAND ;je v eax zprava WM_COMMAND?
-			je near .Command
+    push dword NULL
+    call [ExitProcess]
+    
+; ---------------------- PROCEDURA OKNA ---------------------------------
+    WndProc:
+        WndProc_PARAMBYTES EQU 16
+        enter 0,0
+        
+        mov eax,dword [ebp+12]
+        cmp eax,WM_CREATE
+            je near .Create
+        cmp eax,WM_DESTROY
+            je near .Destroy
+        cmp eax,WM_CLOSE
+            je near .Destroy
+        cmp eax,WM_COMMAND ; je v eax sprava WM_COMMAND?
+            je near .Command
 
-			push dword [ebp+20]
-			push dword [ebp+16]
-			push dword [ebp+12]
-			push dword [ebp+8]
-			call [DefWindowProc]			
-			jmp near .return
+            push dword [ebp+20]
+            push dword [ebp+16]
+            push dword [ebp+12]
+            push dword [ebp+8]
+            call [DefWindowProc]            
+            jmp near .return
 
-	
-	.Create:	
+    
+    .Create:
+        ; Vytvorenie tlacidla
+        push dword NULL
+        push dword [hInstance]
+        push dword NULL
+        push dword [ebp+8]
+        push dword 30
+        push dword 140
+        push dword 210
+        push dword 80
+        push dword WS_CHILD | BS_DEFPUSHBUTTON | WS_VISIBLE | WS_TABSTOP
+        push dword szCButn
+        push dword ClassNameStatButton
+        push dword 0
+        call [CreateWindowEx]
+        mov [hButton], eax
+        jmp near .return
 
-		jmp near .return
+    .Command:
+        ; Kontrola, ci bolo kliknute na tlacidlo
+        mov eax, [hButton]
+        cmp [ebp+20], eax
+        je near .CButton
+        jmp near .return
+        
+    .CButton:
+        ; Zobrazenie dialogoveho okna po kliknuti na tlacidlo
+        push dword MB_OK | MB_ICONINFORMATION
+        push dword cap_dialog
+        push dword mes_dialog
+        push dword NULL
+        call [MessageBox]
+        jmp near .return
 
-	.Command:
-
-		jmp near .return
-			.CButton:
-
-				jmp near .return
-
-
-	.Destroy:
-		push dword 0
-		call [PostQuitMessage]
-		xor eax,eax
-		jmp near .return
-	.return:
-		leave
-		ret WndProc_PARAMBYTES
-; ---------------------- / WINDOW PROCEDURE ---------------------------------
-
+    .Destroy:
+        push dword 0
+        call [PostQuitMessage]
+        xor eax, eax
+        jmp near .return
+        
+    .return:
+        leave
+        ret WndProc_PARAMBYTES
+; ---------------------- / PROCEDURA OKNA ---------------------------------
